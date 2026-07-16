d9f5b9f88ed72eadf22f39d1bde25d305c69f5be
Vic <jvictor.devprofissional@gmail.com>
2026-07-16 19:05:16 -0300

    Adding write_command function

--- ssd1306/include/ssd1306.h
+++ ssd1306/include/ssd1306.h
@@ -2,7 +2,12 @@
 #define SSD1306_H //Marca que esse arquivo já foi incluído. Header Guards.
 
 #include "esp_err.h"
+#include <stdint.h>
 
-esp_err_t ssd1306_init_i2c(void); //Existe uma função com esse nome e essa assinatura.
+esp_err_t ssd1306_init(void); //Existe uma função com esse nome e essa assinatura.
+
+esp_err_t ssd1306_write_command(uint8_t command)
+
+
 
 #endif //Header Guards.
--- ssd1306/ssd1306.c
+++ ssd1306/ssd1306.c
@@ -3,16 +3,17 @@
 #include "driver/i2c_master.h"
 #include "driver/gpio.h"
 
+#include <stdint.h>
+
 static i2c_master_bus_handle_t bus_handle = NULL;
 static i2c_master_dev_handle_t oled_handle = NULL;
 
-uint8_t comando = 0xAE;
-
-
-
-esp_err_t ssd1306_init_i2c(void)
+esp_err_t ssd1306_init(void)
 {
+}
 
+static esp_err_t ssd1306_create_bus(void)
+{
     i2c_master_bus_config_t bus_config = {
         .i2c_port = I2C_NUM_0,
         .sda_io_num = GPIO_NUM_21,
@@ -24,12 +25,14 @@ esp_err_t ssd1306_init_i2c(void)
 
     esp_err_t ret = i2c_new_master_bus(&bus_config, &bus_handle);
 
-    if (ret != ESP_OK){
+    if (ret != ESP_OK)
+    {
         return ret;
     }
+};
 
-    return ESP_OK;
-
+static esp_err_t ssd1306_create_device(void)
+{
     i2c_device_config_t device_config = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
         .device_address = 0x3C,
@@ -38,7 +41,20 @@ esp_err_t ssd1306_init_i2c(void)
 
     i2c_master_bus_add_device(bus_handle, &device_config, &oled_handle);
 
-    i2c_master_transmit(oled_handle, &comando, 1, 1000);
+    i2c_master_transmit(oled_handle, &ssd1306_write_command, 1, 1000);
+}
 
-    
+esp_err_t ssd1306_write_command(uint8_t command)
+{
+
+    if (oled_handle == NULL)
+    {
+        return ESP_ERR_INVALID_STATE;
+    }
+
+    uint8_t buffer[] = {
+        0x00,
+        command};
+
+    i2c_master_receive()
 }
